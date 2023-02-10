2a015ae8586482763b3bea5be9a7dc3a6796b055
Rahul Chaudhary <54467972+rahulchaudhary2244@users.noreply.github.com>
2023-02-10 15:49:36 +0530

    Updating backend url

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Thanks from './components/Thanks';
 export const config = {
     //endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
     //endpoint: `https://qkart-frontend-module.herokuapp.com/api/v1`,
-    endpoint: `https://qkart-node-server.onrender.com/api/v1`,
+    endpoint: `https://qkart-node-server.onrender.com/v1`,
 };
 
 function App() {
